65630646f9e3e1cc3a2b1563ae1524b9eb45b13f
Danut Ilie Leordean <danuwenthome@gmail.com>
2022-10-15 15:52:36 +0300

    Tema lab 9

--- src/Main.java
+++ src/Main.java
@@ -15,13 +15,38 @@ public class Main {
         System.out.println(5+15/3*2-8%3);
 
 
+        int first  = 50;
+        int second = 10;
+        int result = first + second;
+        System.out.println( "rezultatul adunarii dintre " + first + " si " + second+ " este egal cu: " + result);
 
+        int a = 30;
+        int b = 2;
+        int c = a / b;
+        System.out.println("rezultatul impartirii dintre " + a + " si "+ b +" este egal cu: " + c);
 
+        int d = -5;
+        int e = 8;
+        int f = 6;
+        int rezultat = d + e * f;
+        System.out.println("rezultatul operatiei este:"+ rezultat);
 
+        int g = 55;
+        int h = 9;
+        int i = (g + h) % h;
+        System.out.println("rezultatul operatiei este:"+ i);
 
+        int j = 20;
+        int k = -3;
+        int l = 5;
+        int m = j + k * l / e;
+        System.out.println("rezultatul operatiei este:"+ m);
 
-
-
+        int n = 5;
+        int o = 15;
+        int p = 3;
+        int q = n + o / p * b - e / p;
+        System.out.println("rezultatul operatiei este:"+ q);
 
 
 
